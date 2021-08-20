1f50868b1d94c05004cb1231b7c646f231efecf0
Ab <computerm246@gmail.com>
2021-08-21 00:37:58 +0600

    ui design done

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// DOM Selection
+const memoryDefault = document.getElementById("memory-default");
+const memoryOne = document.getElementById("memory-one");
+const storageDefault = document.getElementById("storage-default");
+const storageOne = document.getElementById("storage-one");
+const storageTwo = document.getElementById("storage-two");
+const deliveryDefault = document.getElementById("delivery-default");
+const deliveryOne = document.getElementById("delivery-one");
+const mackPrice = document.getElementById("mack-price");
+const memoryPrice = document.getElementById("memory-price");
+const storagePrice = document.getElementById("storage-price");
+const deliveryPrice = document.getElementById("delivery-price");
+const totalPrice = document.getElementById("total-price");
+const promoCode = document.getElementById("promo-code");
+const submitCode = document.getElementById("submit-code");
+const overallTotal = document.getElementById("overall-price");
